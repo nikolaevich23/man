e90d3f1f3030a502616bf22c38be28f101986918
in1975 <in1975@ya.ru>
2023-05-12 15:56:48 +0400

    up

--- Coldboot/1.bat
+++ Coldboot/1.bat
@@ -3,19 +3,17 @@ rem && chcp 65001
 setlocal enabledelayedexpansion
 if exist pkgi_tunings0.txt del /q pkgi_tunings0.txt
 
-for %%P in (cfw_*.pkg) do (
+for %%P in (*.pkg) do (
 	set sz=%%~zP
 	@CertUtil -hashfile "%%P" SHA256| findstr/v : >sha.tmp
 	set /p sha=<sha.tmp
-	echo CUSTOM-INSTALLER_00-0000000000000000,11,[Coldboot-CFW] %%~nP,Logo and Sound on loading,,https://github.com/nikolaevich23/man/raw/master/Coldboot/%%P,!sz!,!sha!>>pkgi_tunings0.txt
+	set sha=!sha: =!
+ 	set pr=%%P
+	set pr=!pr:~0,3!
+	if !pr!==cfw echo CUSTOM-INSTALLER_00-0000000000000000,11,[Coldboot-CFW] %%~nP,Logo and Sound on loading,,https://github.com/nikolaevich23/man/raw/master/Coldboot/%%P,!sz!,!sha!>>pkgi_tunings0.txt
+	if !pr!==hen echo CUSTOM-INSTALLER_00-0000000000000000,11,[Coldboot-HEN] %%~nP,Logo and Sound on loading,,https://github.com/nikolaevich23/man/raw/master/Coldboot/%%P,!sz!,!sha!>>pkgi_tunings0.txt
 )
 
-for %%P in (hen_*.pkg) do (
-	set sz=%%~zP
-	@CertUtil -hashfile "%%P" SHA256| findstr/v : >sha.tmp
-	set /p sha=<sha.tmp
-	echo CUSTOM-INSTALLER_00-0000000000000000,11,[Coldboot-HEN] %%~nP,Logo and Sound on loading,,https://github.com/nikolaevich23/man/raw/master/Coldboot/%%P,!sz!,!sha!>>pkgi_tunings0.txt
-)
 del /q sha.tmp
 echo Done. See the result in pkgi_tunings0.txt
 echo. & pause
--- Wave/1.bat
+++ Wave/1.bat
@@ -3,19 +3,18 @@ rem && chcp 65001
 setlocal enabledelayedexpansion
 if exist pkgi_tunings0.txt del /q pkgi_tunings0.txt
 
-for %%P in (cfw_*.pkg) do (
+for %%P in (*.pkg) do (
 	set sz=%%~zP
 	@CertUtil -hashfile "%%P" SHA256| findstr/v : >sha.tmp
 	set /p sha=<sha.tmp
-	echo CUSTOM-INSTALLER_00-0000000000000000,11,[Wave-CFW] %%~nP,PS3 Wave qrc,,https://github.com/nikolaevich23/man/raw/master/Wave/%%P,!sz!,!sha!>>pkgi_tunings0.txt
+	set sha=!sha: =!
+ 	set pr=%%P
+	set pr=!pr:~0,3!
+	echo !pr!
+	if !pr!==cfw echo CUSTOM-INSTALLER_00-0000000000000000,11,[Wave-CFW] %%~nP,PS3 Wave qrc,,https://github.com/nikolaevich23/man/raw/master/Wave/%%P,!sz!,!sha!>>pkgi_tunings0.txt
+	if !pr!==hen echo CUSTOM-INSTALLER_00-0000000000000000,11,[Wave-HEN] %%~nP,PS3 Wave qrc,,https://github.com/nikolaevich23/man/raw/master/Wave/%%P,!sz!,!sha!>>pkgi_tunings0.txt
 )
 
-for %%P in (hen_*.pkg) do (
-	set sz=%%~zP
-	@CertUtil -hashfile "%%P" SHA256| findstr/v : >sha.tmp
-	set /p sha=<sha.tmp
-	echo CUSTOM-INSTALLER_00-0000000000000000,11,[Wave-HEN] %%~nP,PS3 Wave qrc,,https://github.com/nikolaevich23/man/raw/master/Wave/%%P,!sz!,!sha!>>pkgi_tunings0.txt
-)
 del /q sha.tmp
 echo Done. See the result in pkgi_tunings0.txt
 echo. & pause
